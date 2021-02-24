59d540de375650073134c407daca81881b09a3dd
Hamish Isaacs <hamisaacs@gmail.com>
2021-02-24 21:56:54 +0800

    Updated burger.js using arrow funcitons

--- models/burger.js
+++ models/burger.js
@@ -2,23 +2,23 @@ const orm = require('../config/orm.js');
 
 const burgers = {
   all(cb) {
-    orm.all('burgers', function(res) {
+    orm.all('burgers', (res) => {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   create(cols, vals, cb) {
-    orm.create('burgers', cols, vals, function(res) {
+    orm.create('burgers', cols, vals, (res) => {
       cb(res);
     });
   },
   update(objColVals, condition, cb) {
-    orm.update('burgers', objColVals, condition, function(res) {
+    orm.update('burgers', objColVals, condition, (res) => {
       cb(res);
     });
   },
   delete(condition, cb) {
-    orm.delete('burgers', condition, function(res) {
+    orm.delete('burgers', condition, (res) => {
       cb(res);
     });
   },
