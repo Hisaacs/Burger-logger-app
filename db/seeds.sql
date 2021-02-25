e8b8081d6a550b788fa40dcf0cdb53b216dc615e
Hamish Isaacs <hamisaacs@gmail.com>
2021-02-25 15:41:56 +0800

    Added burger_style.css and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(name, devoured) VALUES ('Bacon & Egg Burger', false);
+INSERT INTO burgers(name, devoured) VALUES ('Ultimate Whopper', false);
+INSERT INTO burgers(name, devoured) VALUES ('Chicken & Mayo', false);
