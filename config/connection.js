47db4e4e34b68ba8e878161b50dc6c6d491f710f
Hamish Isaacs <hamisaacs@gmail.com>
2021-02-27 12:13:55 +0800

    Added JawsDB connnection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,11 @@
 const mysql = require('mysql');
-
+const dsn = "mysql://sjhfagm0irjhw8xe:v7s35e2nbnn960si@qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xhi5e4l9m4w33r5n"
 const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'is@h@m123',
-  database: 'burger_db',
+  host:"qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user:"sjhfagm0irjhw8xe",
+  password:"v7s35e2nbnn960si",
+  database:"xhi5e4l9m4w33r5n",
 });
-
-// Make connection.
 connection.connect((err) => {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
@@ -17,6 +13,5 @@ connection.connect((err) => {
   }
   console.log(`connected as id ${connection.threadId}`);
 });
-
 // Export connection for our ORM to use.
 module.exports = connection;
